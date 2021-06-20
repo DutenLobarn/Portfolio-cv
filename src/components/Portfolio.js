8947135976b196ac55e2cd69b7534cff4584ebc5
Mandus Lindström <mandus85@hotmail.com>
2021-06-20 16:24:06 +0200

    Email left

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -55,10 +55,13 @@ export default function Portfolio() {
                 {toggleProject1 ? 
                 <div className="cover">
                     <div className="portfolioToggleContainer">
-                        <h2>Exempel 1</h2>
+                        <h2>Storage Rental</h2>
                         <div className="divider"></div>
-                        <img className="portfolioToggleImg" src={storagerental} alt='Example1'></img>
-                        <p className="portfolioP">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Mollitia neque assumenda ipsam nihil, molestias magnam, recusandae quos quis inventore quisquam velit asperiores, vitae? Reprehenderit soluta, eos quod consequuntur itaque. Nam</p>
+                        <a href="https://github.com/DutenLobarn/StorageRentalPage" target="_blank"  rel="noreferrer">
+                            <img className="portfolioToggleImg" src={storagerental} alt='Storage Rental'></img>
+                        </a>
+                        <p className="portfolioP">A webpage for my brother.</p>
+                        <p>In this projekt my goal was to brush up on my React and Redux skills and also automatize email corresponding with potentional costumer.</p>
                         <button className="portfolioButton" onClick={handletoggleProject1}>Close Window</button>
                     </div>
                 </div>
@@ -68,10 +71,14 @@ export default function Portfolio() {
                 {toggleProject2 ? 
                 <div className="cover">
                     <div className="portfolioToggleContainer">
-                        <h2>Exempel 2</h2>
+                        <h2>The Landloard</h2>
                         <div className="divider"></div>
-                        <img className="portfolioToggleImg" src={landloard} alt='Example2'></img>
-                        <p className="portfolioP">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Mollitia neque assumenda ipsam nihil, molestias magnam, recusandae quos quis inventore quisquam velit asperiores, vitae? Reprehenderit soluta, eos quod consequuntur itaque. Nam</p>
+                        <a href="https://github.com/DutenLobarn/App-for-a-landlord-company" target="_blank"  rel="noreferrer">
+                            <img className="portfolioToggleImg" src={landloard} alt='Example2'></img>
+                        </a>
+                        <p className="portfolioP">Also a webpage for my brother.</p>
+                        <p>In this projekt my goal was to brush up on my HTML and CSS skills and also play around with the CSS.</p>
+                        <p>Far from done!</p>
                         <button className="portfolioButton" onClick={handletoggleProject2}>Close Window</button>
                     </div>
                 </div>
@@ -81,10 +88,14 @@ export default function Portfolio() {
                 {toggleProject3 ? 
                 <div className="cover">
                     <div className="portfolioToggleContainer">
-                        <h2>Exempel 3</h2>
+                        <h2>Node Teamwork</h2>
                         <div className="divider"></div>
-                        <img className="portfolioToggleImg" src={sinuswebshop} alt='Example3'></img>
-                        <p className="portfolioP">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Mollitia neque assumenda ipsam nihil, molestias magnam, recusandae quos quis inventore quisquam velit asperiores, vitae? Reprehenderit soluta, eos quod consequuntur itaque. Nam</p>
+                        <a href="https://github.com/DutenLobarn/nackademin-slutprojekt-backend-Grupparbete-Domus" target="_blank"  rel="noreferrer">
+                            <img className="portfolioToggleImg" src={sinuswebshop} alt='Example3'></img>
+                        </a>
+                        <p className="portfolioP">Final School Exam in our Node JS Course</p>
+                        <p>Me and another student got a finished FrontEnd (Vue) and worked with Node JS, Mongoose and MongoDB for the school exam.</p>
+                        <p>Very fun project and awesome collaboration!</p>
                         <button className="portfolioButton" onClick={handletoggleProject3}>Close Window</button>
                     </div>
                 </div>
@@ -94,10 +105,13 @@ export default function Portfolio() {
                 {toggleProject4 ? 
                 <div className="cover">
                     <div className="portfolioToggleContainer">
-                        <h2>Exempel 4</h2>
+                        <h2>React Teamwork</h2>
                         <div className="divider"></div>
-                        <img className="portfolioToggleImg" src={airbean} alt='Example4'></img>
-                        <p className="portfolioP">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Mollitia neque assumenda ipsam nihil, molestias magnam, recusandae quos quis inventore quisquam velit asperiores, vitae? Reprehenderit soluta, eos quod consequuntur itaque. Nam</p>
+                        <a href="https://github.com/Avancerad-JavaScript-med-React-JS-EC/grupparbete-golden-rangers" target="_blank"  rel="noreferrer">
+                            <img className="portfolioToggleImg" src={airbean} alt='Example4'></img>
+                        </a>
+                        <p className="portfolioP">Final School Exam in our React Course</p>
+                        <p>Me and 3 students got a Figma sketch that our final result should look like. First time using testing. Easy exercise i think.</p>
                         <button className="portfolioButton" onClick={handletoggleProject4}>Close Window</button>
                     </div>
                 </div>
@@ -107,10 +121,13 @@ export default function Portfolio() {
                 {toggleProject5 ? 
                 <div className="cover">
                     <div className="portfolioToggleContainer">
-                        <h2>Exempel 5</h2>
+                        <h2>Booking Site</h2>
                         <div className="divider"></div>
-                        <img className="portfolioToggleImg" src={conserts} alt='Example5'></img>
-                        <p className="portfolioP">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Mollitia neque assumenda ipsam nihil, molestias magnam, recusandae quos quis inventore quisquam velit asperiores, vitae? Reprehenderit soluta, eos quod consequuntur itaque. Nam</p>
+                        <a href="https://github.com/Avancerad-JavaScript-med-React-JS-EC/inlamningsuppgift-4-DutenLobarn" target="_blank"  rel="noreferrer">
+                            <img className="portfolioToggleImg" src={conserts} alt='Example5'></img>
+                        </a>
+                        <p className="portfolioP">A simple school project  using React.
+                        Teacher gave us 3 Figma pages that we should make with code. </p>
                         <button className="portfolioButton" onClick={handletoggleProject5}>Close Window</button>
                     </div>
                 </div>
@@ -120,10 +137,13 @@ export default function Portfolio() {
                 {toggleProject6 ? 
                 <div className="cover">
                     <div className="portfolioToggleContainer">
-                        <h2>Exempel 6</h2>
+                        <h2>Registration</h2>
                         <div className="divider"></div>
-                        <img className="portfolioToggleImg" src={registrationform} alt='Example6'></img>
-                        <p className="portfolioP">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Mollitia neque assumenda ipsam nihil, molestias magnam, recusandae quos quis inventore quisquam velit asperiores, vitae? Reprehenderit soluta, eos quod consequuntur itaque. Nam</p>
+                        <a href="https://github.com/Avancerad-JavaScript-med-React-JS-EC/inlamningsuppgift-3-DutenLobarn" target="_blank"  rel="noreferrer">
+                            <img className="portfolioToggleImg" src={registrationform} alt='Example6'></img>
+                        </a>
+                        <p className="portfolioP">Also a school project, like other school projects we got some Figma pages to code with React.</p> 
+                        <p>Our first time using Redux.</p>
                         <button className="portfolioButton" onClick={handletoggleProject6}>Close Window</button>
                     </div>
                 </div>
